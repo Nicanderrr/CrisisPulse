8fc09f380a4b1c268d7f4c61bdd56f4c2f472700
Nicanderrr <nicanderarkomensah@gmail.com>
2026-09-19 10:17:54 +0000

    Isolate tests from production database

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,9 +4,27 @@ namespace Tests;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function createApplication()
+    {
+        $app = parent::createApplication();
+
+        config([
+            'database.default' => 'sqlite',
+            'database.connections.sqlite.database' => ':memory:',
+            'cache.default' => 'array',
+            'session.driver' => 'array',
+        ]);
+
+        DB::purge('sqlite');
+        DB::reconnect('sqlite');
+
+        return $app;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
